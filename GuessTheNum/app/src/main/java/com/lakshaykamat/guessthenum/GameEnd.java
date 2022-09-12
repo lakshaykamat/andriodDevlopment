0d3e08bf50a16eb63dc30ef53382c3ef9393c9c1
Lakshay <Lakshaykamat2048@gmail.com>
2022-09-12 11:42:29 +0530

    new

---
+++ GuessTheNum/app/src/main/java/com/lakshaykamat/guessthenum/GameEnd.java
@@ -0,0 +1,14 @@
+package com.lakshaykamat.guessthenum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameEnd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_end);
+    }
+}
