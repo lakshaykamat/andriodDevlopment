7094c74b33987e092e523763944884a438bc7b22
Lakshay <Lakshaykamat2048@gmail.com>
2022-09-12 13:55:58 +0530

    bug fixed

--- GuessTheNum/app/src/main/java/com/lakshaykamat/guessthenum/GameEnd.java
+++ GuessTheNum/app/src/main/java/com/lakshaykamat/guessthenum/GameEnd.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -18,10 +19,12 @@ public class GameEnd extends AppCompatActivity {
         TextView gameEndHeading = findViewById(R.id.gameEndHeading);
         TextView movesLeft = findViewById(R.id.movesLeft);
         TextView moveUsed = findViewById(R.id.movesUsed);
+        TextView randomNum = findViewById(R.id.randomNum);
 
         String endHeading = intent.getStringExtra(MainActivity.EXTRA_USER_WINS);
         String left = intent.getStringExtra(MainActivity.EXTRA_MOVES_LEFT);
         String used = intent.getStringExtra(MainActivity.EXTRA_MOVES_USED);
+        String Num = intent.getStringExtra(MainActivity.EXTRA_RANDOM_NUM);
 
         if (endHeading.equals("true")){
             gameEndHeading.setText("YOU WIN");
@@ -29,7 +32,11 @@ public class GameEnd extends AppCompatActivity {
             gameEndHeading.setText("YOU LOSE");
         }
 
+        randomNum.setText("The Number was: " + Num);
         movesLeft.setText("Moves Left: " + left);
         moveUsed.setText("Move Used: " + used);
+    }
+    public void retryGame(){
+        Intent intent = new Intent(this,MainActivity.class);
     }
 }
--- GuessTheNum/app/src/main/java/com/lakshaykamat/guessthenum/MainActivity.java
+++ GuessTheNum/app/src/main/java/com/lakshaykamat/guessthenum/MainActivity.java
@@ -15,34 +15,42 @@ import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
     //Variables
-    public int movesLeft = 3;
+    public int movesLeft = 7;
     public int movesUsed = 0;
     public Boolean usrWins = false;
     private EditText usrNumEl;
+    private TextView textViewMovesLeft;
+    private TextView resultEl;
     public static final String EXTRA_MOVES_LEFT = "com.lakshaykamat.guessnum.movesleft";
     public static final String EXTRA_MOVES_USED = "com.lakshaykamat.guessnum.movesused";
     public static final String EXTRA_USER_WINS = "com.lakshaykamat.guessnum.usrwins";
+    public static final String EXTRA_RANDOM_NUM = "com.lakshaykamat.guessnum.radomnum";
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Button button = findViewById(R.id.button);
-        TextView resultEl = findViewById(R.id.resultEl);
+        resultEl = findViewById(R.id.resultEl);
         usrNumEl = findViewById(R.id.usrNumEl);
+        textViewMovesLeft = findViewById(R.id.textViewMoviesLeft);
+
+        //Showing moves left on game loads
+        textViewMovesLeft.setText("Total Moves: "+ movesLeft);
+
 
         DecimalFormat df = new DecimalFormat("#");
         double randomNum = Math.floor((Math.random() * 100  + 1));
 
         //formatting decimal places and converting to String
         String randomNumToString = df.format(randomNum);
-        //resultEl.setText(randomNumToString);
-        //Converting to Random Num Integer
+        //Converting to randomNumToString Integer
         int randomNumToInt = Integer.parseInt(randomNumToString);
 
 
-        //On click listener on button
+        //On click listener event on button
         button.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
             @Override
@@ -50,13 +58,13 @@ public class MainActivity extends AppCompatActivity {
                     //Converting user entered number to a String
                     String usrNumToString = usrNumEl.getText().toString();
 
-                    //if user didn't enter any number shows this error
+                    //if user didn't enter any number shows this error and return the function
                     if (usrNumToString.equals("")) {
                         usrNumEl.setError("Enter a number");
                         return;
                     }
 
-                    //converting String to integer for comparison
+                    //converting user entered String to integer for comparison
                     int usrNumToInt = Integer.parseInt(usrNumToString);
 
                     //if user enter more than 100 or less than or equal to 0 show error
@@ -64,49 +72,48 @@ public class MainActivity extends AppCompatActivity {
                         Toast.makeText(MainActivity.this, "Choose between [1 to 100]", Toast.LENGTH_SHORT).show();
                     }
 
-                    //Checking Conditions
-                    if (usrNumToInt == randomNumToInt) {
-                        resultEl.setText("You wins");
-                        usrNumEl.setText("");
+                    //Checking Conditions or Comparing the numbers
+                    if (usrNumToInt == randomNumToInt) {//if usrNum and randomNum is same usrWins = true
                         usrWins = true;
-                        movesUsed++;
-                        creatingNewActivity(intToString(movesLeft),intToString(movesUsed), Boolean.toString(usrWins));
+                        movesUsed++;//One move used
+                        movesLeft--;//Lost one move
+                        usrNumEl.setText("");//removing old number in input filed
+                        textViewMovesLeft.setText("Moves: "+ movesLeft);
+                        //creating the new activity and passing the arguments
+                        creatingNewActivity(Integer.toString(movesLeft),Integer.toString(movesUsed),Boolean.toString(usrWins),randomNumToString);
                     } else if (usrNumToInt > randomNumToInt) {
-                        resultEl.setText("Choose a Lower number");
-                        usrNumEl.setText("");
-                       movesLeft--;
-                       movesUsed++;
-                    } else if (usrNumToInt < randomNumToInt) {
-                        resultEl.setText("Choose a Higher Number");
-                        usrNumEl.setText("");
                         movesLeft--;
                         movesUsed++;
+                        resultEl.setText("Choose a Lower number");
+                        usrNumEl.setText("");
+                        textViewMovesLeft.setText("Moves: "+ movesLeft);
+                    } else if (usrNumToInt < randomNumToInt) {
+                        movesLeft--;
+                        movesUsed++;
+                        resultEl.setText("Choose a Higher Number");
+                        usrNumEl.setText("");
+                        textViewMovesLeft.setText("Moves: " + movesLeft);
                     } else {
                         Toast.makeText(MainActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
                     }
 
-                    zeroMoves(movesLeft, intToString(movesUsed),Boolean.toString(usrWins));
+                    //if user has been used his all moves then end the game
+                    zeroMoves(movesLeft, Integer.toString(movesUsed),Boolean.toString(usrWins),randomNumToString);
                 }
         });
     }
-    public void zeroMoves(int movesLeft, String movesUsed , String usrWins) {
+    public void zeroMoves(int movesLeft, String movesUsed , String usrWins,String randomNum) {
         if(movesLeft == 0){
             Toast.makeText(MainActivity.this, "0 Moves left", Toast.LENGTH_SHORT).show();
-            String LEFT = intToString(movesLeft);
-            creatingNewActivity(LEFT,movesUsed,usrWins);
-            //return;
+            creatingNewActivity(Integer.toString(movesLeft),movesUsed,usrWins,randomNum);
         }
-        //Toast.makeText(MainActivity.this, "END", Toast.LENGTH_SHORT).show();
     }
-    public void creatingNewActivity(String movesLeft,String moveUsed,String usrWins){
+    public void creatingNewActivity(String movesLeft,String moveUsed,String usrWins,String randomNum){
         Intent intent = new Intent(this,GameEnd.class);
         intent.putExtra(EXTRA_MOVES_LEFT,movesLeft);
         intent.putExtra(EXTRA_MOVES_USED,moveUsed);
         intent.putExtra(EXTRA_USER_WINS, usrWins);
+        intent.putExtra(EXTRA_RANDOM_NUM, randomNum);
         startActivity(intent);
     }
-    public String intToString(int integer){
-        return Integer.toString(integer);
-    }
-
 }
